6603b3daaba24d571aa0fed596efefd2cab13713
Nicolae Basso <nickbassotrue@gmail.com>
2022-06-07 20:44:23 +0300

    update reducers

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,6 @@
 export const users = [
   {
+    id: '13189713-1d68-42af-a947-b0285d1c1ce8',
     name: 'Jake Johnson',
     email: 'jj@gmail.com',
     password: '4321'
@@ -11,3 +12,17 @@ export const devices = [];
 export const pets = [];
 
 export const plans = [];
+
+// 14400 seconds = 4 hours...
+export const SCHEDULE_TIME = [
+  { id: '46d55c40-fda5-4078-b744-01ce673e0e32', time: 21600 },
+  { id: '09b0823f-34ad-40c6-90e0-8f0fc305f59c', time: 43200 },
+  { id: 'a9ed6426-a619-4095-80a5-7d4aa4a3b705', time: 64800 }
+];
+export const DEFAULT_LOCATION = 'main';
+export const FOOD_TYPES = { dry: 'dry', wet: 'wet' };
+export const DEFAULT_MENU = {
+  id: '43c64ee1-ffba-427c-bd07-c1728e122eb4',
+  name: 'Pro Plan',
+  type: FOOD_TYPES.dry
+};
---
+++ src/reducers/petSlice.js
@@ -0,0 +1,39 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { UUIDGeneratorBrowser } from '../utils';
+
+const initialState = {
+  pets: []
+};
+
+export const petSlice = createSlice({
+  name: 'pet',
+  initialState: { ...initialState },
+  reducers: {
+    addPet: (state, action) => {
+      const { name } = action.payload;
+
+      state.pets = [...state.pets, { id: UUIDGeneratorBrowser(), name }];
+    },
+
+    updatePet: (state, action) => {
+      const { id, name = state.name, device = state.device } = action.payload;
+
+      state.pets.forEach((pet) => {
+        if (pet.id === id) {
+          pet.name = name;
+          pet.device = device;
+        }
+      });
+    },
+
+    removePet: (state, action) => {
+      const { id } = action.payload;
+
+      state.pets = [...state.pets.filter((pet) => pet.id !== id)];
+    }
+  }
+});
+
+export const { addPet, removePet } = petSlice.actions;
+
+export default petSlice.reducer;
