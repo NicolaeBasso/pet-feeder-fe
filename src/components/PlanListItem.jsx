b47f894f2065f85259c75681cd3b01c21323562f
Nicolae Basso <nickbassotrue@gmail.com>
2022-06-09 00:20:59 +0300

    init food plans

--- src/components/PlanListItem.jsx
+++ src/components/PlanListItem.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import deviceImg from '../icons/device.png';
 import { Box, Button, Container, Grid, Pagination, Typography } from '@mui/material';
 import { useState } from 'react';
 import FormControlLabel from '@mui/material/FormControlLabel';
@@ -8,10 +7,10 @@ import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { updateDevice } from '../reducers/deviceSlice';
+import { updatePlan } from '../reducers/planSlice';
 import { DEFAULT_LOCATIONS, DEFAULT_PLANS } from '../constants';
 
-export const DeviceListItem = ({ device }) => {
+export const PlanListItem = ({ plan }) => {
   const dispatch = useDispatch();
   const plans = useSelector((state) => state.plan.plans);
 
@@ -21,28 +20,28 @@ export const DeviceListItem = ({ device }) => {
         <div className="flex-1">
           <li className=" m-2 px-3 pt-3">
             <Typography variant="h4" fontWeight="bold" mb={3} color="#0096c7">
-              {device.name}
+              {plan.name}
             </Typography>
           </li>
           <li
             className=" capitalize text-xl font-bold m-2 px-3 pt-3"
-            onChange={() => dispatch(updateDevice({ ...device, power: !device.power }))}>
+            onChange={() => dispatch(updatePlan({ ...plan, power: !plan.power }))}>
             <FormControlLabel
               sx={{
                 display: 'block'
               }}
-              control={<Switch checked={device.power} name="loading" color="primary" />}
+              control={<Switch checked={plan.power} name="loading" color="primary" />}
               label="Power"
             />
           </li>
           <li className="capitalize text-xl font-bold m-2 px-3 pt-3">
             <Autocomplete
-              value={device.location}
+              value={plan.location}
               disablePortal
               id="combo-box-demo"
               options={DEFAULT_LOCATIONS}
               onChange={(e) => {
-                dispatch(updateDevice({ ...device, location: e.target.textContent }));
+                dispatch(updatePlan({ ...plan, location: e.target.textContent }));
               }}
               sx={{ width: 300 }}
               renderInput={(params) => {
@@ -52,7 +51,7 @@ export const DeviceListItem = ({ device }) => {
           </li>
           <li className=" capitalize text-xl font-bold m-2 px-3 pt-3 ">
             <Autocomplete
-              value={device.plan?.name}
+              value={plan.plan?.name}
               disablePortal
               id="combo-box-demo"
               options={DEFAULT_PLANS.map((plan) => plan.name)}
@@ -61,7 +60,7 @@ export const DeviceListItem = ({ device }) => {
                   return el.name === e.target.outerText;
                 });
 
-                dispatch(updateDevice({ ...device, plan: plans.length ? selected[0] : null }));
+                dispatch(updatePlan({ ...plan, plan: plans.length ? selected[0] : null }));
               }}
               sx={{ width: 300 }}
               renderInput={(params) => {
@@ -71,9 +70,7 @@ export const DeviceListItem = ({ device }) => {
           </li>
         </div>
         <div className="md:px-2 mt-3 md:mt-0">
-          <Link
-            className="inline-block  mx-5 mb-4 px-2 py-2 rounded-md"
-            to={`/plans/${device.id}/`}>
+          <Link className="inline-block  mx-5 mb-4 px-2 py-2 rounded-md" to={`/plans/${plan.id}/`}>
             <Button fullWidth variant="contained" type="submit">
               See plan
             </Button>
