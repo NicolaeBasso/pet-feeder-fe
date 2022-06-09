878979e69842566a785125836a2cb02222b80e43
Nicolae Basso <nickbassotrue@gmail.com>
2022-06-09 09:24:19 +0300

    center plans

--- src/components/Plans.jsx
+++ src/components/Plans.jsx
@@ -3,20 +3,21 @@ import { Typography } from '@mui/material';
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { PlanListItem } from './PlanListItem';
+import { Box } from '@mui/material';
 
 export const Plans = () => {
   const plans = useSelector((state) => state.plan.plans);
 
   return (
     <>
-      <div>
+      <Box sx={{ display: 'flex', justifyContent: 'center' }}>
         <Typography>Your plans</Typography>
         <ul className="planCard">
           {plans?.map((plan) => (
             <PlanListItem key={plan.id} plan={plan} />
           ))}
         </ul>
-      </div>
+      </Box>
     </>
   );
 };
